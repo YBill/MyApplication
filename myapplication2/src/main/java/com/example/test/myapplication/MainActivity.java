41dab04baeaa81786abd0550f19e88d22cf71f2b
Bill <weibiao@17shihui.com>
2015-11-18 15:52:36 +0800

    add click show textview

--- myapplication2/src/main/java/com/example/test/myapplication/MainActivity.java
+++ myapplication2/src/main/java/com/example/test/myapplication/MainActivity.java
@@ -4,11 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button button;
+    private EditText editText;
+    private TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +20,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.content_main);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
+        editText = (EditText) findViewById(R.id.editText);
+        textview = (TextView) findViewById(R.id.textview);
+
     }
 
     @Override
     public void onClick(View v) {
+        textview.setText(editText.getText().toString());
         Toast.makeText(this, "click Button", Toast.LENGTH_SHORT).show();
     }
 }
