7f37cf20a7352012f3943ebd43df211ab4f640fb
ywb <weibiao@17shihui.com>
2016-05-20 23:01:33 +0800

    test ..

--- myapplication2/src/main/java/com/example/test/myapplication/MainActivity.java
+++ myapplication2/src/main/java/com/example/test/myapplication/MainActivity.java
@@ -2,32 +2,60 @@ package com.example.test.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import com.facebook.drawee.backends.pipeline.Fresco;
 
-    private Button button;
-    private EditText editText;
-    private TextView textview;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements LiveStaffView.OnStaffClickListener {
+
+    private LiveStaffView liveStaffView, liveStaffView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(this);
-        editText = (EditText) findViewById(R.id.editText);
-        textview = (TextView) findViewById(R.id.textview);
+        Fresco.initialize(getApplicationContext());
+        setContentView(R.layout.activity_test);
+        liveStaffView = (LiveStaffView) findViewById(R.id.view_livestaff);
+        liveStaffView.setLiveView(LiveStaffView.LiveMode.single, null);
+        liveStaffView.setAnchorMode(LiveStaffView.AnchorMode.onMike, "");
+        liveStaffView.setOnStaffClickListener(this);
+        liveStaffView2 = (LiveStaffView) findViewById(R.id.view_livestaff2);
+        liveStaffView2.setLiveView(LiveStaffView.LiveMode.doubles, getData());
+        liveStaffView2.setAnchorMode(LiveStaffView.AnchorMode.other, "主播直播");
+        liveStaffView2.setOnStaffClickListener(this);
 
     }
 
+    private List<String> getData(){
+        List<String> list = new ArrayList<String>();
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        return list;
+    }
+
     @Override
-    public void onClick(View v) {
-        textview.setText(editText.getText().toString());
-        Toast.makeText(this, "click Button", Toast.LENGTH_SHORT).show();
+    public void onAudienceClick() {
+        Toast.makeText(this, "听众头像", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onFristAnchorClick() {
+        Toast.makeText(this, "第一个主播", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSecondAnchorClick() {
+        Toast.makeText(this, "第二个主播", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMikeClick(LiveStaffView.AnchorMode mode) {
+        Toast.makeText(this, "mode:" + mode, Toast.LENGTH_SHORT).show();
     }
 }
